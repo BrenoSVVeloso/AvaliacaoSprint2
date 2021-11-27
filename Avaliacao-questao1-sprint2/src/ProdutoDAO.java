9694d66db9f3fdf71e4337089dc2df9ac2105138
Breno Veloso <bbreno.valadares@gmail.com>
2021-11-27 15:21:12 -0300

    Adicionando comentarios

--- Avaliacao-questao1-sprint2/src/ProdutoDAO.java
+++ Avaliacao-questao1-sprint2/src/ProdutoDAO.java
@@ -7,13 +7,15 @@ import java.sql.Statement;
 public class ProdutoDAO {
     
     private Connection connection;
-
+    //recebe a conexão com o banco de dados e adiciona ao seu atributo connection
     public ProdutoDAO(Connection connection){
         this.connection = connection;
     }
 
+    //Cadastra o produto
     public void salvar(Produto produto) throws SQLException{
         int ordenacao = 0;
+        //Pega o valor maior da coluna ORDENACAO
         String sql = "SELECT MAX(ORDENACAO) AS MAXORDENACAO FROM PRODUTO";
         try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
             preparedStatement.execute();
@@ -23,7 +25,7 @@ public class ProdutoDAO {
             }
         }
 
-
+        //insere o produto na Tabela Produto
         sql = "INSERT INTO PRODUTO (nome, descricao, preco, ordenacao, quantidade) VALUES (?, ?, ?, ?, ?)";
 
         try(PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
@@ -37,14 +39,14 @@ public class ProdutoDAO {
             preparedStatement.execute();
 
             try(ResultSet resultset = preparedStatement.getGeneratedKeys()){
-            
+                // insere deu ID
                 while(resultset.next()){
                     produto.setId(resultset.getInt(1));
                 }
             }
         }
     }
-
+    //Exclui o segundo produto da Tabela Produto
     public void excluirSegundoProduto()throws SQLException{
         
         String sql = "DELETE FROM PRODUTO WHERE ORDENACAO = 2";
@@ -57,12 +59,14 @@ public class ProdutoDAO {
                 System.out.println("Segundo produto excluido com sucesso");
             }
         }
+        //reorganiza a coluna ORDENAÇÃO
         sql = "UPDATE PRODUTO SET ORDENACAO = ORDENACAO - 1 WHERE ORDENACAO > 2";
         try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
             preparedStatement.execute();
         }
     }
 
+    //Atualiza o primeiro produto da tabela produto
     public void atualizarPrimeiroProduto(String nome, String descricao, double preco, int quantidade) throws SQLException{
         String sql = "UPDATE PRODUTO SET NOME = ?, DESCRICAO = ?, PRECO = ?, QUANTIDADE = ? WHERE ORDENACAO = 1";
 
